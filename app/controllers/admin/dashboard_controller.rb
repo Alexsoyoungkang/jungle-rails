e43d9e094fb442052ad99ddb96ca3d27dc4230f4
Soyoung <ksyoung3424@gmail.com>
2023-10-03 02:14:07 +0000

    Display admin dashboard counts for products and catergories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   
   def show
+    @product_count = Product.count # creates instant variables for a count of how many products are in the database
+    @category_count = Category.count # same for category - they are sent to show.html.erb in views/admin/dashboard
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all.order(created_at: :desc)
+    @products = Product.all.order(created_at: :desc) # @products instant variable is sent to index.html.erb template file
   end
 
   def show
-    @product = Product.find params[:id]
+    @product = Product.find params[:id] # @products instant variable is sent to show.html.erb template file
   end
 
 end
